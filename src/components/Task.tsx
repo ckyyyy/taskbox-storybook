90cf0c18ff1ab48344f6a3d2f186cbe55f6bd136
ckyyyy <chongkyxxx@gmail.com>
2021-03-08 17:19:01 +0800

    feat: add Task component with stories

---
+++ src/components/Task.tsx
@@ -0,0 +1,44 @@
+import React, { FC } from 'react';
+
+type Task = {
+  id: string;
+  title: string;
+  state: string;
+  updatedAt?: Date;
+}
+
+type TaskProps = {
+  task: Task;
+  onArchiveTask: (id: string) => void;
+  onPinTask: (id: string) => void;
+}
+
+const Task: FC<TaskProps> = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
+  return (
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === 'TASK_ARCHIVED'}
+          disabled={true}
+          name="checked"
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+      </label>
+      <div className="title">
+        <input type="text" value={title} readOnly={true} placeholder="Input title" />
+      </div>
+
+      <div className="actions" onClick={event => event.stopPropagation()}>
+        {state !== 'TASK_ARCHIVED' && (
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+          <a onClick={() => onPinTask(id)}>
+            <span className={`icon-star`} />
+          </a>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Task;
