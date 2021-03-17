c8d59bf307fef7896162fb875f61297325ef7f24
ckyyyy <chongkyxxx@gmail.com>
2021-03-17 09:46:01 +0800

    feat: add TaskList component with stories

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,72 @@
+import React, { FC } from 'react';
+
+import Task from './Task';
+import { TaskItem } from '../types/Task';
+
+type TaskProps = {
+  loading: boolean;
+  tasks: TaskItem[];
+  onArchiveTask: (id: string) => void;
+  onPinTask: (id: string) => void;
+};
+
+const TaskList: FC<TaskProps> = ({
+  loading,
+  tasks,
+  onArchiveTask,
+  onPinTask,
+}) => {
+  const events = {
+    onPinTask,
+    onArchiveTask,
+  };
+
+  const LoadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>Loading</span> <span>cool</span> <span>state</span>
+      </span>
+    </div>
+  );
+
+  const tasksInOrder = [
+    ...tasks.filter((t) => t.state === 'TASK_PINNED'),
+    ...tasks.filter((t) => t.state !== 'TASK_PINNED'),
+  ];
+
+  if (loading) {
+    return (
+      <div className="list-items">
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+      </div>
+    );
+  }
+
+  if (tasks.length === 0) {
+    return (
+      <div className="list-items">
+        <div className="wrapper-message">
+          <span className="icon-check" />
+          <div className="title-message">You have no tasks</div>
+          <div className="subtitle-message">Sit back and relax</div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="list-items">
+      {tasksInOrder.map((task) => (
+        <Task key={task.id} task={task} {...events} />
+      ))}
+    </div>
+  );
+};
+
+export default TaskList;
